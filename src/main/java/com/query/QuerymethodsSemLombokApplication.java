27d577a65aa6d3b81daa906c84587d650102f749
Adriano Elias Daniel <adrianoedaniel@gmail.com>
2025-05-13 00:17:51 -0300

    Teste sem modulo teste

---
+++ src/main/java/com/query/QuerymethodsSemLombokApplication.java
@@ -0,0 +1,13 @@
+package com.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerymethodsSemLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerymethodsSemLombokApplication.class, args);
+	}
+
+}
